440c5c83d905dbe4bb3988369b05c1acae895127
BorcanRaluca <borcanraluca50@gmail.com>
2024-12-15 19:34:41 +0200

    Added pom.xml

--- src/test/java/ism/sqm/AddItemTest.java
+++ src/test/java/ism/sqm/AddItemTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ManagerAddItemTest {
+public class AddItemTest {
 
     @Test
     public void testAddItem() {
